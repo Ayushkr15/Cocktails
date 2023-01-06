7b4a2c7dd5985384eb2b05687b50a413644eb6d2
Ayush Kumar Sinha <ayushkr1322@gmail.com>
2023-01-06 14:57:01 +0530

    Initial Commit

---
+++ src/pages/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title"> About Us </h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In pharetra
+        lobortis nunc. Cras ac dolor non velit varius varius tincidunt ac diam.
+        Sed in imperdiet neque, sed tempus felis. Maecenas eu suscipit ex.
+        Pellentesque pulvinar lectus vitae felis iaculis, sed mattis risus
+        auctor. Fusce feugiat porttitor felis ut congue. Vestibulum vulputate
+        mollis mauris vitae placerat. Integer placerat mattis pharetra. Morbi ut
+        libero ac felis fermentum varius.
+      
+      </p>
+    </section>
+  );
+}
+
+export default About
